7f8deaa16661d0515b75c952bf9c3ad5c09d6810
Dialber <dialberob@gmail.com>
2021-09-05 17:24:48 +0100

    Normalize adicionado y  ajustes de maquetacion al login

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { FileComponent } from './components/shared/file/file.component';
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    FileComponent
   ],
   imports: [
     CommonModule,
--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  names:string[]=["correo","password"];
   constructor() { }
 
   ngOnInit(): void {
